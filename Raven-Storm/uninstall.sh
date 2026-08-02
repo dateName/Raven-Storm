62a8b37d8fb79066d474e8718b767dd875fe6f6f
(o_o)b <102200975+dateName@users.noreply.github.com>
2026-08-02 10:49:53 +0800

    Add files via upload

--- Raven-Storm/uninstall.sh
+++ Raven-Storm/uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "[i] We will now uninstall Raven-Storm..."
-echo "[i] This will delete all backups."
+echo "[i] 正在卸载 Raven-Storm..."
+echo "[i] 警告：此操作将永久删除所有备份和数据."
 sudo rm -i /usr/bin/rst
 sudo rm -rf -i /usr/share/Raven-Storm
 
-echo "[i] Raven-Storm sucessfully uninstalled."
+echo "[i] RRaven-Storm 卸载完成."
 exit 0
